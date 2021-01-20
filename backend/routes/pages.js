e9740f9a18d30f3987755216c17bb63d84db370a
verneleem <themaster5_07@yahoo.com>
2021-01-20 15:03:29 -0600

    create operations for front end.
    delete backend models, unused controllers and routes

--- backend/routes/pages.js
+++ backend/routes/pages.js
@@ -1,24 +1,8 @@
 const express = require("express");
 
-const isAuth = require("../middleware/isAuth");
 const pagesController = require("../controllers/pages");
 
 const router = express.Router();
-
-// GET /pages
-router.get("/", isAuth, pagesController.getPages);
-
-// GET /pages/{id}
-router.get("/:pageId", isAuth, pagesController.getPage);
-
-// POST /pages
-router.post("/", isAuth, pagesController.postPage);
-
-// PUT /pages/{id}
-router.put("/:pageId", isAuth, pagesController.putPage);
-
-// DELETE /pages/{id}
-router.delete("/:pageId", isAuth, pagesController.deletePage);
 
 // POST /pages/images
 router.post("/images", pagesController.postImage);
